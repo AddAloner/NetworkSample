b16fe03a57141ba239e1eec23b2d295d45f67d24
Alexey Yachmenov <aloner.brn@gmail.com>
2015-06-05 10:38:56 +0300

    Initial Commit

---
+++ NetworkSampleTests/NetworkSampleTests.m
@@ -0,0 +1,40 @@
+//
+//  NetworkSampleTests.m
+//  NetworkSampleTests
+//
+//  Created by Alexey Yachmenov on 05.06.15.
+//  Copyright (c) 2015 Alexey Yachmenov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface NetworkSampleTests : XCTestCase
+
+@end
+
+@implementation NetworkSampleTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
